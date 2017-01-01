9e78a7605a90347fef43c7c445ead11e0933686b
Clinton Blackburn <clinton.blackburn@gmail.com>
2017-01-01 16:00:48 -0500

    Updated subway intent
    Users must now specify a destination station instead of inbound/outbound. The inbound/outbound concept (as it relates to Park Street) doesn't seem to apply to the API.

--- alexa_transit_skill.py
+++ alexa_transit_skill.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 import datetime
+import itertools
 import logging
 import math
 import os
@@ -11,6 +12,7 @@ from flask_ask import Ask, statement
 from pytz import timezone
 
 app = Flask(__name__)
+# app.config['ASK_VERIFY_REQUESTS'] = False
 ask = Ask(app, '/')
 logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
@@ -22,10 +24,70 @@ TIMEZONE = timezone('US/Eastern')
 APP_NAME = 'MBTA'
 MBTA_API_KEY = os.environ['MBTA_API_KEY']
 
+STATIONS = {
+    'airport': 'place-aport',
+    'alewife': 'place-alfcl',
+    'andrew': 'place-andrw',
+    'aquarium': 'place-aqucl',
+    'ashmont': 'place-asmnl',
+    'assembly': 'place-astao',
+    'back bay': 'place-bbsta',
+    'beachmont': 'place-bmmnl',
+    'bowdoin': 'place-bomnl',
+    'braintree': 'place-brntn',
+    'broadway': 'place-brdwy',
+    'central': 'place-cntsq',
+    'charles m.g.h.': 'place-chmnl',
+    'chinatown': 'place-chncl',
+    'community college': 'place-ccmnl',
+    'davis': 'place-davis',
+    'downtown crossing': 'place-dwnxg',
+    'fields corner': 'place-fldcr',
+    'forest hills': 'place-forhl',
+    'government center': 'place-gover',
+    'green street': 'place-grnst',
+    'harvard': 'place-harsq',
+    'haymarket': 'place-haecl',
+    'jackson square': 'place-jaksn',
+    'jfk/umass': 'place-jfk',
+    'kendall/mit': 'place-knncl',
+    'malden center': 'place-mlmnl',
+    'massachusetts ave.': 'place-masta',
+    'maverick': 'place-mvbcl',
+    'north quincy': 'place-nqncy',
+    'north station': 'place-north',
+    'oak grove': 'place-ogmnl',
+    'orient heights': 'place-orhte',
+    'park street': 'place-pktrm',
+    'porter': 'place-portr',
+    'quincy adams': 'place-qamnl',
+    'quincy center': 'place-qnctr',
+    'revere beach': 'place-rbmnl',
+    'roxbury crossing': 'place-rcmnl',
+    'ruggles': 'place-rugg',
+    'savin hill': 'place-shmnl',
+    'shawmut': 'place-smmnl',
+    'south station': 'place-sstat',
+    'state street': 'place-state',
+    'stony brook': 'place-sbmnl',
+    'suffolk downs': 'place-sdmnl',
+    'sullivan square': 'place-sull',
+    'tufts medical center': 'place-tumnl',
+    'wellington': 'place-welln',
+    'wollaston': 'place-wlsta',
+    'wonderland': 'place-wondl',
+    'wood island': 'place-wimnl',
+}
+
 
 def distance(p1, p2):
     """ Returns the distance between two points. """
     return math.sqrt(pow((p2[0] - p1[0]), 2) + pow((p2[1] - p1[1]), 2))
+
+
+class NoPredictionsError(Exception):
+    """ Raised if predictions cannot be determined for a route/stop. """
+    pass
 
 
 class MbtaApi(object):
@@ -37,6 +99,7 @@ class MbtaApi(object):
     API_HOST = 'http://realtime.mbta.com/developer/api/v2'
     INBOUND = '1'
     OUTBOUND = '0'
+    SUBWAY_ROUTE_TYPE = '1'
 
     def __init__(self, latitude, longitude):
         self.api_key = MBTA_API_KEY
@@ -49,67 +112,158 @@ class MbtaApi(object):
             'outbound': cls.OUTBOUND,
         }[s]
 
-    def stops_by_route(self, route_id, direction):
+    def stops_by_route(self, route_id):
         # TODO Cache this information for all users
         url = '{api_host}/stopsbyroute?api_key={api_key}&route={route}&format=json'.format(
             api_host=self.API_HOST, api_key=self.api_key, route=route_id
         )
-        stops_by_route = requests.get(url).json()['direction']
-        stops_by_route = [stops for stops in stops_by_route if stops['direction_id'] == direction][0]['stop']
-        return stops_by_route
+        return requests.get(url).json()
 
-    def next_departure(self, route_id, direction):
-        stops_by_route = self.stops_by_route(route_id, direction)
+    def get_closest_stop(self, stops):
+        """ Given a list of stops, returns the stop closest to the current location.
 
-        # Determine the closest stop
+        Returns:
+            (distance, stop)
+        """
         closest = (float('inf'), None)
 
-        for stop in stops_by_route:
+        for stop in stops:
             stop_location = (float(stop['stop_lat']), float(stop['stop_lon']))
             stop_distance = distance(self.current_location, stop_location)
             if stop_distance < closest[0]:
                 closest = (stop_distance, stop)
 
-        stop_id = closest[1]['stop_id']
+        return closest
+
+    def next_predicted_departures(self, stop_id, route_id, direction_id):
+        route_id = route_id.lower()
+        direction_id = int(direction_id)
         url = '{api_host}/predictionsbystop?api_key={api_key}&stop={stop_id}&format=json'.format(
             api_host=self.API_HOST, api_key=self.api_key, stop_id=stop_id
         )
         predictions = requests.get(url).json()
-        stop = predictions['stop_name']
-        prediction = predictions['mode'][0]['route'][0]['direction'][0]['trip'][0]
-        prediction = datetime.datetime.fromtimestamp(int(prediction['pre_dt']), tz=TIMEZONE)
-        return stop, prediction
+
+        # Ensure we have predictions
+        if 'mode' not in predictions:
+            raise NoPredictionsError
+
+        # Get the route we actually care about
+        routes = list(itertools.chain.from_iterable([mode['route'] for mode in predictions['mode']]))
+        routes = [route for route in routes if route['route_id'].lower() == route_id]
+
+        if not routes:
+            raise NoPredictionsError
+
+        # Get the desired direction of the route
+        trips = []
+        for direction in routes[0]['direction']:
+            if int(direction['direction_id']) == direction_id:
+                trips = direction['trip']
+                break
+
+        predictions = []
+        for trip in trips[:3]:
+            predictions.append(datetime.datetime.fromtimestamp(int(trip['pre_dt']), tz=TIMEZONE))
+
+        predictions.sort()
+        return predictions
+
+    def next_bus_departure(self, route_id, direction_id):
+        stops_by_route = self.stops_by_route(route_id)
+        stops_by_route = stops_by_route['direction']
+        stops_by_route = [stops for stops in stops_by_route if stops['direction_id'] == direction_id][0]['stop']
+
+        # Determine the closest stop
+        __, closest_stop = self.get_closest_stop(stops_by_route)
+        stop_id = closest_stop['stop_id']
+        prediction = self.next_predicted_departures(stop_id, route_id, direction_id)
+
+        # TODO Format street types properly
+        stop_name = closest_stop['stop_name'].replace(' St ', ' St. ')
+        return stop_name, prediction
+
+    def next_subway_departure(self, route_id, destination_station):
+        directions = self.stops_by_route(route_id)['direction']
+        closest = (float('inf'), None, None)
+
+        for direction in directions:
+            candidates = []
+            for stop in direction['stop']:
+                if stop['parent_station'] == destination_station:
+                    break
+                candidates.append(stop)
+
+            distance, stop = self.get_closest_stop(candidates)
+            if distance < closest[0]:
+                closest = distance, stop, int(direction['direction_id'])
+
+        closest_stop = closest[1]
+
+        if closest_stop:
+            direction_id = closest[2]
+            prediction = self.next_predicted_departures(closest_stop['stop_id'], route_id, direction_id)
+            return closest_stop['parent_station_name'], prediction
+        else:
+            # TODO Raise an exception!
+            return None, None
+
+
+def handle_no_predictions_error():
+    text = 'I was unable to locate a departure time.'
+    return statement(text).simple_card(APP_NAME, text)
+
+
+def format_predictions(predictions):
+    departure_times = [departure_time.strftime('%I:%M %p') for departure_time in predictions]
+
+    if len(departure_times) == 1:
+        spoken_departures = departure_times[0]
+    else:
+        spoken_departures = ', '.join(departure_times[:-1]) + ', and ' + departure_times[-1]
+
+    return spoken_departures
 
 
 @ask.intent('BusIntent')
 def bus(direction, route):
     mbta_api = MbtaApi(LATITUDE, LONGITUDE)
     int_direction = mbta_api.convert_direction(direction)
-    location, departure = mbta_api.next_departure(route, int_direction)
-    spoken_departure = departure.strftime('%I:%M %p')
+    try:
+        location, departure_times = mbta_api.next_bus_departure(route, int_direction)
+    except NoPredictionsError:
+        return handle_no_predictions_error()
+
+    spoken_departures = format_predictions(departure_times)
 
     text = 'The next {direction} {route} bus departs {location} at {departure}.'.format(
-        direction=direction, route=route, location=location, departure=spoken_departure
+        direction=direction, route=route, location=location, departure=spoken_departures
     )
     return statement(text).simple_card(APP_NAME, text)
 
 
 @ask.intent('SubwayIntent')
-def subway(direction, subway_line):
+def subway(subway_line, destination_station):
     # TODO Determine how to differentiate between the different green lines (B, C, D, E)
     if subway_line == 'green':
         text = 'I cannot provide times for the green line.'
         return statement(text).simple_card(APP_NAME, text)
 
+    parent_station = STATIONS.get(destination_station.lower())
+    if not parent_station:
+        text = 'I am was unable to locate a station named {station}.'.format(station=destination_station)
+        return statement(text).simple_card(APP_NAME, text)
+
     mbta_api = MbtaApi(LATITUDE, LONGITUDE)
-    int_direction = mbta_api.convert_direction(direction)
+    try:
+        departure_station, departure_times = mbta_api.next_subway_departure(subway_line, parent_station)
+    except NoPredictionsError:
+        return handle_no_predictions_error()
 
-    # TODO Determine if the user's meaning of outbound/inbound corresponds with that of the MBTA.
-    location, departure = mbta_api.next_departure(subway_line, int_direction)
-    spoken_departure = departure.strftime('%I:%M %p')
+    spoken_departures = format_predictions(departure_times)
 
-    text = 'The next {direction} {subway_line} line train departs {location} at {departure}.'.format(
-        direction=direction, subway_line=subway_line, location=location, departure=spoken_departure
+    text = 'The next {subway_line} line trains to {destination_station} depart {departure_station} at {departure_times}.'.format(
+        subway_line=subway_line, destination_station=destination_station, departure_station=departure_station,
+        departure_times=spoken_departures
     )
     return statement(text).simple_card(APP_NAME, text)
 
